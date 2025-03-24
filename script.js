6535f6d07c45eb2b86b35c78d738cf9dd990dfe2
pedro20075 <163026026+pedro20075@users.noreply.github.com>
2025-03-24 14:34:36 -0300

    Add files via upload
    24/03/25 Realização dos códigos no Visual studio

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEverntListener('DOMContentLoaded',function()){
+    const aumentaFonteBotão = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotão.addEventListener('click', function){
+        tamanhoAtualFonte+= 0.1;
+        document.body.s
+
+    }
+}
