a4be49025e477adeb938a7b3f25a45a6d532e7bc
pedro20075 <163026026+pedro20075@users.noreply.github.com>
2025-06-02 13:30:05 -0300

    Add files via upload
    projeto finalizado 30/05/25

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-document.addEverntListener('DOMContentLoaded',function(){
-    const aumentaFonteBotão = document.getElementById('aumentar-fonte');
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
-    aumentaFonteBotão.addEventListener('click', function(){
-        tamanhoAtualFonte+= 0.1;
-        document.body.style.fontSize='${tamanhoAtualFonte}rem';
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
 
-    diminuiFonteBotao.addEventListener('click',function(){
-        tamanhoAtualFonte-= 0.1;
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem';
-    });
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
+    });
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     botaoDeAcessibilidade.addEventListener('click', function (){
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-       })
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+     alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+    
+    });
+    ScrollReveal().reveal('#inicio', { delay: 500 });
+    ScrollReveal().reveal('#tropicalia', { delay: 500 });
+    ScrollReveal().reveal('#galeria', { delay: 500 });
+    ScrollReveal().reveal('#contato', { delay: 500 });
+       
 });
